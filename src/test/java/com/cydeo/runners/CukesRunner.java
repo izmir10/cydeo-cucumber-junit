0ad023f9345040aa0a626ff94d9dfe81dbdea762
Enis <87227909+izmir10@users.noreply.github.com>
2022-06-25 18:12:23 +0200

    June 25, 2022 Live Class

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -15,8 +15,8 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
         dryRun = false,
-        tags = "@apple",
-        publish = true
+        tags = "",
+        publish = false
 
 
 )
